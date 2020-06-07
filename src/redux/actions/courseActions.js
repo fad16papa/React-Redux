05560c541b7520ab389483c0bcb358f497c4f209
fad16papa <fad16papa@gmail.com>
2020-06-07 18:19:50 +0800

    React-Redux first push

---
+++ src/redux/actions/courseActions.js
@@ -0,0 +1,60 @@
+import * as types from "./actionTypes";
+import * as courseApi from "../../api/courseApi";
+import { beginApiCall, apiCallError } from "./apiStatusActions";
+
+export function loadCourseSuccess(courses) {
+  return { type: types.LOAD_COURSES_SUCCESS, courses };
+}
+
+export function createdCourseSuccess(course) {
+  return { type: types.CREATE_COURSE_SUCCESS, course };
+}
+
+export function updateCourseSuccess(course) {
+  return { type: types.UPDATE_COURSE_SUCCESS, course };
+}
+
+export function deleteCourseOptmistic(course) {
+  return { type: types.DELETE_COURSE_OPTIMISTIC, course };
+}
+
+export function loadCourses() {
+  return function (dispatch) {
+    dispatch(beginApiCall());
+    return courseApi
+      .getCourses()
+      .then((courses) => {
+        dispatch(loadCourseSuccess(courses));
+      })
+      .catch((error) => {
+        dispatch(apiCallError(error));
+        throw error;
+      });
+  };
+}
+
+export function saveCourse(course) {
+  return function (dispatch, getState) {
+    dispatch(beginApiCall());
+    return courseApi
+      .saveCourse(course)
+      .then((savedCourse) => {
+        course.id
+          ? dispatch(updateCourseSuccess(savedCourse))
+          : dispatch(createdCourseSuccess(savedCourse));
+      })
+      .catch((error) => {
+        dispatch(apiCallError(error));
+        throw error;
+      });
+  };
+}
+
+export function deleteCourse(course) {
+  return function (dispatch) {
+    // Doing optimistic delete, so not dispatching bgein/end api call
+    // actions, or apiCallError action  since we're not showing the loading status for this.
+    dispatch(deleteCourseOptmistic(course));
+    return courseApi.deleteCourse(course.id);
+  };
+}
